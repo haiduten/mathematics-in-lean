2c9013de6f8fa4642f4b244d09fa52c19d767c1e
Dimiter <d.y.radonov@gmail.com>
2026-03-25 15:14:20 -0400

    Add my_files

---
+++ my_files/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -0,0 +1,160 @@
+import Mathlib.Analysis.SpecialFunctions.Log.Basic
+import MIL.Common
+
+variable (a b c d e : ℝ)
+open Real
+
+#check (le_refl : ∀ a : ℝ, a ≤ a)
+#check (le_trans : a ≤ b → b ≤ c → a ≤ c)
+
+section
+variable (h : a ≤ b) (h' : b ≤ c)
+
+#check (le_refl : ∀ a : Real, a ≤ a)
+#check (le_refl a : a ≤ a)
+#check (le_trans : a ≤ b → b ≤ c → a ≤ c)
+#check (le_trans h : b ≤ c → a ≤ c)
+#check (le_trans h h' : a ≤ c)
+
+end
+
+example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z := by
+  apply le_trans
+  · apply h₀
+  · apply h₁
+
+example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z := by
+  apply le_trans h₀
+  apply h₁
+
+example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z :=
+  le_trans h₀ h₁
+
+example (x : ℝ) : x ≤ x := by
+  apply le_refl
+
+example (x : ℝ) : x ≤ x :=
+  le_refl x
+
+#check (le_refl : ∀ a, a ≤ a)
+#check (le_trans : a ≤ b → b ≤ c → a ≤ c)
+#check (lt_of_le_of_lt : a ≤ b → b < c → a < c)
+#check (lt_of_lt_of_le : a < b → b ≤ c → a < c)
+#check (lt_trans : a < b → b < c → a < c)
+
+-- Try this.
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
+  apply lt_of_le_of_lt
+  · exact h₀
+  · apply lt_trans
+    · exact h₁
+    · apply lt_of_le_of_lt
+      · apply h₂
+      · apply h₃
+
+
+
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
+  linarith
+
+section
+
+example (h : 2 * a ≤ 3 * b) (h' : 1 ≤ a) (h'' : d = 2) : d + a ≤ 5 * b := by
+  linarith
+
+end
+
+example (h : 1 ≤ a) (h' : b ≤ c) : 2 + a + exp b ≤ 3 * a + exp c := by
+  linarith [exp_le_exp.mpr h']
+
+#check (exp_le_exp : exp a ≤ exp b ↔ a ≤ b)
+#check (exp_lt_exp : exp a < exp b ↔ a < b)
+#check (log_le_log : 0 < a → a ≤ b → log a ≤ log b)
+#check (log_lt_log : 0 < a → a < b → log a < log b)
+#check (add_le_add : a ≤ b → c ≤ d → a + c ≤ b + d)
+#check (add_le_add_left : a ≤ b → ∀ c, c + a ≤ c + b)
+#check (add_le_add_right : a ≤ b → ∀ c, a + c ≤ b + c)
+#check (add_lt_add_of_le_of_lt : a ≤ b → c < d → a + c < b + d)
+#check (add_lt_add_of_lt_of_le : a < b → c ≤ d → a + c < b + d)
+#check (add_lt_add_left : a < b → ∀ c, c + a < c + b)
+#check (add_lt_add_right : a < b → ∀ c, a + c < b + c)
+#check (add_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a + b)
+#check (add_pos : 0 < a → 0 < b → 0 < a + b)
+#check (add_pos_of_pos_of_nonneg : 0 < a → 0 ≤ b → 0 < a + b)
+#check (exp_pos : ∀ a, 0 < exp a)
+#check add_le_add_left
+
+example (h : a ≤ b) : exp a ≤ exp b := by
+  rw [exp_le_exp]
+  exact h
+
+example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
+  apply add_lt_add_of_lt_of_le
+  · apply add_lt_add_of_le_of_lt h₀
+    apply exp_lt_exp.mpr h₁
+  apply le_refl
+
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left
+  exact h₀
+
+example : (0 : ℝ) < 1 := by norm_num
+
+example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    · norm_num
+    · exact exp_pos a
+  apply log_le_log h₀
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  exact h
+
+example : 0 ≤ a ^ 2 := by
+  -- apply?
+  exact sq_nonneg a
+
+example (h : a ≤ b) : c - exp b ≤ c - exp a := by
+  apply sub_le_sub_left
+  apply exp_le_exp.mpr
+  exact h
+
+example : 2*a*b ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 - 2*a*b + b^2
+  calc
+    a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+
+  calc
+    2*a*b = 2*a*b + 0 := by ring
+    _ ≤ 2*a*b + (a^2 - 2*a*b + b^2) := add_le_add (le_refl _) h
+    _ = a^2 + b^2 := by ring
+
+example : 2*a*b ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 - 2*a*b + b^2
+  calc
+    a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
+example : |a*b| ≤ (a^2 + b^2)/2 := by
+  apply abs_le'.mpr
+  constructor
+  have h: (0: ℝ) < 2 := by norm_num
+  apply (mul_le_mul_right h).mp
+  have h₁: 0 ≤ (a ^ 2 + b ^ 2) / 2 * 2 - a * b * 2
+  calc
+    (a ^ 2 + b ^ 2) / 2 * 2 - a * b * 2 = (a ^ 2 + b ^ 2) - a * b * 2 := by ring
+    _ = (a - b) ^2 := by ring
+    _ ≥ 0 := pow_two_nonneg (a - b)
+  linarith
+  have h: (0: ℝ) < 2 := by norm_num
+  apply (mul_le_mul_right h).mp
+  have h₁: 0 ≤ (a ^ 2 + b ^ 2) / 2 * 2 + a * b * 2
+  calc
+    (a ^ 2 + b ^ 2) / 2 * 2 + a * b * 2 = (a ^ 2 + b ^ 2) + a * b * 2 := by ring
+    _ = (a + b) ^2 := by ring
+    _ ≥ 0 := pow_two_nonneg (a + b)
+  linarith
---
+++ my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -0,0 +1,131 @@
+import MIL.Common
+import Mathlib.Data.Real.Basic
+
+namespace C02S04
+
+section
+variable (a b c d : ℝ)
+
+#check (min_le_left a b : min a b ≤ a)
+#check (min_le_right a b : min a b ≤ b)
+#check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
+
+example : min a b = min b a := by
+  apply le_antisymm
+  · show min a b ≤ min b a
+    apply le_min
+    · apply min_le_right
+    apply min_le_left
+  · show min b a ≤ min a b
+    apply le_min
+    · apply min_le_right
+    apply min_le_left
+
+example : min a b = min b a := by
+  have h : ∀ x y : ℝ, min x y ≤ min y x := by
+    intro x y
+    apply le_min
+    apply min_le_right
+    apply min_le_left
+  apply le_antisymm
+  apply h
+  apply h
+
+example : min a b = min b a := by
+  apply le_antisymm
+  repeat
+    apply le_min
+    apply min_le_right
+    apply min_le_left
+
+example : max a b = max b a := by
+  apply ge_antisymm
+  repeat
+    apply max_le
+    · apply le_max_right
+    · apply le_max_left
+
+example : min (min a b) c = min a (min b c) := by
+  apply le_antisymm
+  · apply le_min
+    · show min (min a b) c ≤ a
+      have h₀ := min_le_left a b
+      have h₁ := min_le_left (min a b) c
+      exact le_trans h₁ h₀
+    · show min (min a b) c ≤ min b c
+      apply le_min
+      · show min (min a b) c ≤ b
+        have h₀ := min_le_right a b
+        have h₁ := min_le_left (min a b) c
+        exact le_trans h₁ h₀
+      · exact min_le_right (min a b) c
+  · apply le_min
+    · apply le_min
+      · exact min_le_left a (min b c)
+      · show min a (min b c) ≤ b
+        have h₀ := min_le_left b c
+        have h₁ := min_le_right a (min b c)
+        exact le_trans h₁ h₀
+    · show min a (min b c) ≤ c
+      have h₀ := min_le_right b c
+      have h₁ := min_le_right a (min b c)
+      exact le_trans h₁ h₀
+
+theorem aux : min a b + c ≤ min (a + c) (b + c) := by
+  apply le_min
+  · show min a b + c ≤ a + c
+    apply add_le_add_right
+    exact min_le_left a b
+  · show min a b + c ≤ b + c
+    apply add_le_add_right
+    exact min_le_right a b
+
+example : min a b + c = min (a + c) (b + c) := by
+  apply le_antisymm
+  · exact aux a b c
+  · show min (a + c) (b + c) ≤ min a b + c
+    apply sub_le_iff_le_add.mp
+    apply le_min
+    · show min (a + c) (b + c) - c ≤ a
+      apply sub_le_iff_le_add.mpr
+      exact min_le_left (a + c) _
+    · show min (a + c) (b + c) - c ≤ b
+      apply sub_le_iff_le_add.mpr
+      exact min_le_right _ (b + c)
+
+#check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
+
+example : |a| - |b| ≤ |a - b| := by
+  have h1 := abs_add (a - b) b
+  rw[sub_add_cancel] at h1
+  exact sub_le_iff_le_add.mpr h1
+end
+
+section
+variable (w x y z : ℕ)
+
+example (h₀ : x ∣ y) (h₁ : y ∣ z) : x ∣ z :=
+  dvd_trans h₀ h₁
+
+example : x ∣ y * x * z := by
+  apply dvd_mul_of_dvd_left
+  apply dvd_mul_left
+
+example : x ∣ x ^ 2 := by
+  apply dvd_mul_left
+
+example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
+  sorry
+end
+
+section
+variable (m n : ℕ)
+
+#check (Nat.gcd_zero_right n : Nat.gcd n 0 = n)
+#check (Nat.gcd_zero_left n : Nat.gcd 0 n = n)
+#check (Nat.lcm_zero_right n : Nat.lcm n 0 = 0)
+#check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
+
+example : Nat.gcd m n = Nat.gcd n m := by
+  sorry
+end
